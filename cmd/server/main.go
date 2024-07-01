42480052e90366b592b577f1f32ac885c5f77a3f
randyli <155058195@qq.com>
2024-07-01 21:56:23 +0800

    add yaml config

---
+++ cmd/server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net"
+	"os"
+
+	"github.com/expbuild/expbuild/pkg/ac"
+	"github.com/expbuild/expbuild/pkg/cas"
+	"github.com/expbuild/expbuild/pkg/cas/store"
+	"github.com/expbuild/expbuild/pkg/config"
+	"github.com/expbuild/expbuild/pkg/exe"
+	"github.com/expbuild/expbuild/pkg/log"
+	pbbs "github.com/expbuild/expbuild/pkg/proto/gen/bytestream"
+	pb "github.com/expbuild/expbuild/pkg/proto/gen/remote_execution"
+	"google.golang.org/grpc"
+)
+
+var (
+	addr = flag.String("addr", "127.0.0.1:50051", "The exe server address")
+)
+
+// Version indicates the current version of the application.
+var Version = "1.0.0"
+
+var flagConfig = flag.String("config", "./config/local.yml", "path to the config file")
+
+func main() {
+	flag.Parse()
+
+	logger := log.New().With(context.Background(), "version", Version)
+
+	// load application configurations
+	cfg, err := config.Load(*flagConfig, logger)
+	if err != nil {
+		logger.Errorf("failed to load application configuration: %s", err)
+		os.Exit(-1)
+	}
+
+	lis, err := net.Listen("tcp", cfg.ServerHost)
+	if err != nil {
+		logger.Errorf("failed to listen: %v", err)
+		return
+	}
+	s := grpc.NewServer()
+	exe_svr, err := exe.MakeExeServer()
+	if err != nil {
+		logger.Errorf("exe server init error %v", err)
+		panic(1)
+	}
+
+	cas_svr := cas.CASService{
+		Store: store.MakeRedisStore(),
+	}
+
+	ac_svr := ac.NewActionCache()
+
+	pb.RegisterContentAddressableStorageServer(s, &cas_svr)
+	pbbs.RegisterByteStreamServer(s, &cas_svr)
+	pb.RegisterExecutionServer(s, exe_svr)
+	pb.RegisterActionCacheServer(s, ac_svr)
+	exe_svr.Start()
+	logger.Infof("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		logger.Errorf("failed to serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,15 +6,19 @@ replace github.com/bazelbuild/remote-apis/build/bazel/semver => github.com/expbu
 
 require (
 	github.com/alicebob/miniredis v2.5.0+incompatible
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.3.0
+	github.com/qiangxue/go-env v1.0.1
 	github.com/rabbitmq/amqp091-go v1.5.0
 	github.com/rs/zerolog v1.28.0
 	github.com/stretchr/testify v1.8.1
+	go.uber.org/zap v1.27.0
 	google.golang.org/genproto v0.0.0-20221207170731-23e4bf6bdc37
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
@@ -28,6 +32,7 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/yuin/gopher-lua v1.1.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
 	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.4.0 // indirect
